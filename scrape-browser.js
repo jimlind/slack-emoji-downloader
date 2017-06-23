e6ef87c18ead3dabbd01f593d00d2d1470053b8c
Jim Lind <spoon.vw@gmail.com>
2017-06-23 08:25:08 -0400

    Timestamp on Downloaded File.

--- scrape-browser.js
+++ scrape-browser.js
@@ -1,14 +1,3 @@
-/*
- * STOLEN FROM:
- * https://gist.github.com/liamnewmarch/b415c72740090fb548d23553fe5d8810
- */
- 
-/*
- * Step 1:
- * Navigate to https://{your-team}.slack.com/customize/emoji
- * and paste this into dev tools to generate an emoji.json
- */
-
 (function() {
   const rows = document.querySelectorAll('.emoji_row');
   const emoji = Array.from(rows).map(node => {
@@ -17,8 +6,9 @@
     return { name, url };
   });
   const json = JSON.stringify(emoji);
+  const now = new Date().getTime();
   const link = document.createElement('a');
   link.href = `data:text/json,${json}`;
-  link.download = 'emoji.json';
+  link.download = `emoji-${now}.json`;
   link.click();
 })();
